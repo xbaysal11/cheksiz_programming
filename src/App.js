d27f9518bbef3f909de279c0eaf69299b5e12913
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-06-14 05:21:30 +0600

    :seedling: ADDED learning methods section

--- src/App.js
+++ src/App.js
@@ -647,6 +647,23 @@ class App extends Component {
             </div>
           </div>
         </section>
+        <section className="ftco-section-parallax">
+          <div className="parallax-img d-flex align-items-center">
+            <div className="container">
+              <div className="row d-flex justify-content-center">
+                <div className="col-md-7 text-center heading-section heading-section-white ftco-animate">
+                  <h2>Методика обучения</h2>
+                  <p>
+                    Методика обучения: лекции и презентации, практические
+                    занятия, разработка проектов.
+                    <br />
+                    Язык проведения курса - русский и кыргызский.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
 
         <section className="ftco-section testimony-section bg-light">
           <div className="container">

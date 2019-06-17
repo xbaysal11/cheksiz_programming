8d7dbb24479039a3732e6aa79d23d92479de1d76
VladimirZhdanov <VladimirZhdanov1992@gmail.com>
2019-06-17 09:03:16 +0600

    Example&&&

--- src/components/Goals.jsx
+++ src/components/Goals.jsx
@@ -6,6 +6,30 @@ import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 library.add(fab, fas);
 
+const Item = ({ title, description, icon, color }) => (
+  <div className="col-md-8 col-lg-4 d-flex align-self-stretch ftco-animate">
+    <div className="media block-6 services d-block text-center">
+      <div className="d-flex justify-content-center">
+        <div
+          className={`icon color-${color} d-flex justify-content-center mb-3`}
+        >
+          <span>
+            <FontAwesomeIcon
+              icon={icon}
+              className="align-self-center"
+              size="3x"
+            />
+          </span>
+        </div>
+      </div>
+      <div className="media-body p-2 mt-3">
+        <h3 className="heading">{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  </div>
+);
+
 export default class Goals extends Component {
   render() {
     return (
@@ -27,79 +51,42 @@ export default class Goals extends Component {
             </div>
           </div>
           <div className="row">
-            <div className="col-md-8 col-lg-4 d-flex align-self-stretch ftco-animate">
-              <div className="media block-6 services d-block text-center">
-                <div className="d-flex justify-content-center">
-                  <div className="icon color-3 d-flex justify-content-center mb-3">
-                    <span>
-                      <FontAwesomeIcon
-                        icon={["fas", "lightbulb"]}
-                        className="align-self-center"
-                        size="3x"
-                      />
-                    </span>
-                  </div>
-                </div>
-                <div className="media-body p-2 mt-3">
-                  <h3 className="heading">Старт проекта</h3>
-                  <p>
-                    Проект начал свою работу с января 2018 года, охватывает все
+            <Item
+              title="Старт проекта"
+              description="Проект начал свою работу с января 2018 года, охватывает все
                     регионы Кыргызстана и направлен на обучение навыков,
                     востребованных на рынке труда для молодежи с особыми
                     потребностями.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-8 col-lg-4 d-flex align-self-stretch ftco-animate">
-              <div className="media block-6 services d-block text-center">
-                <div className="d-flex justify-content-center">
-                  <div className="icon color-1 d-flex justify-content-center mb-3">
-                    <span>
-                      <FontAwesomeIcon
-                        icon={["fas", "laptop"]}
-                        className="align-self-center"
-                        size="3x"
-                      />
-                    </span>
-                  </div>
-                </div>
-                <div className="media-body p-2 mt-3">
-                  <h3 className="heading">Долгосрочная цель</h3>
-                  <p>
-                    Внесение вклада в сокращение уровня безработицы, особенно
+"
+              icon={["fas", "lightbulb"]}
+              color="1"
+            />
+            <Item
+              title="Долгосрочная цель"
+              description="
+Внесение вклада в сокращение уровня безработицы, особенно
                     среди таких социальных групп, как люди с особыми
                     потребностями, и подготовка профессиональных работников,
                     которые будут способствовать социально-экономическому
                     развитию Кыргызской Республики.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-8 col-lg-4 d-flex align-self-stretch ftco-animate">
-              <div className="media block-6 services d-block text-center">
-                <div className="d-flex justify-content-center">
-                  <div className="icon color-2 d-flex justify-content-center mb-3">
-                    <span>
-                      <FontAwesomeIcon
-                        icon={["fas", "cog"]}
-                        className="align-self-center"
-                        size="3x"
-                      />
-                    </span>
-                  </div>
-                </div>
-                <div className="media-body p-2 mt-3">
-                  <h3 className="heading">Краткосрочная цель</h3>
-                  <p>
+
+"
+              icon={["fas", "laptop"]}
+              color="2"
+            />
+            <Item
+              title="Краткосрочная цель"
+              description="
+
                     Продвижение обучения в сфере IT для людей с особыми
                     потребностями и повышение их навыков для трудоустройства.
                     Данный проект является первым в КР по обучению ЛОВЗ
                     программированию.
-                  </p>
-                </div>
-              </div>
-            </div>
+                  
+"
+              icon={["fas", "cog"]}
+              color="3"
+            />
           </div>
         </div>
       </section>
